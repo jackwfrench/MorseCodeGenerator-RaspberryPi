bcbdc425d434f9f4d8956620ab8ea45771deca37
Jack French <jackfrench45@gmail.com>
2020-01-13 23:38:36 +1000

    updates to readme and code documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MorseCodeGenerator-RaspberryPi
-A morse code generator for raspberry pi connected piezo buzzer
+A morse code text to sound generator for a raspberry pi connected piezo buzzer.
+
+## Motivation 
+Got really bored doing software projects and thought it would be 
+interesting to dive into some simple hardware projects, especially 
+ones that include technological history.
+
+## GPIO and Hardware configuration
+Connect GPIO pin 23 (pin 16) to the positive terminal of a piezo buzzer and ground (pin 6, 9, 14 ... etc) to negative terminal of the buzzer.
+
+## Running the generator 
+To run the morse code generator simply
+```
+cd MorseCodeGenerator-RaspberryPi
+sudo python3 morsecode.py
+```
+Enter alphabetic text then press enter.
+
+## License 
+This project is licensed under the MIT License - Read more at [LICENSE.md](./LICENSE.md)
+
--- morsecode.py
+++ morsecode.py
@@ -2,36 +2,56 @@ import RPi.GPIO as GPIO
 import time
 import json
 
+# default dot and dash times (seconds)
 DOTTIME = 0.0375
 DASHTIME = 0.1125
 
+# setup GPIO and make GPIO 23 output
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 buzzer=23
 GPIO.setup(buzzer, GPIO.OUT)
 
 def playDotSound():
+	"""
+	Plays the dot sound according to specified time
+	"""
 	GPIO.output(buzzer, GPIO.HIGH)
 	time.sleep(DOTTIME)
 	GPIO.output(buzzer, GPIO.LOW)
 	
 def playDashSound():
+	"""
+	Plays the dash sound accord to specified time
+	"""
 	GPIO.output(buzzer, GPIO.HIGH)
 	time.sleep(DASHTIME)
 	GPIO.output(buzzer, GPIO.LOW)
 
 def waitDot():
+	"""
+	Waits the specified dot sound time
+	"""
 	time.sleep(DOTTIME)
 
 def waitDash():
+	"""
+	Waits the specified dash sound time
+	"""
 	time.sleep(DASHTIME)
 
 def decodeMorse(file):
+	"""
+	Decodes the morse json file (eg codes.json)
+	"""
 	with open(file, 'r') as f:
 		morseDict = json.load(f)
 	return morseDict
 
 def playMorseLetter(letter):
+	"""
+	Plays the morse code of a letter
+	"""
 	codes = list(letter)
 	for code in codes:
 		if code == '.':
@@ -41,6 +61,9 @@ def playMorseLetter(letter):
 		waitDash()
 
 def playMorseWord(word):
+	"""
+	Plays the morse code of a word
+	"""
 	for letter in word:
 		playMorseLetter(letter)
 		print(letter + " ")
@@ -49,6 +72,9 @@ def playMorseWord(word):
 	waitDot()
 
 def transcodeWord(word, morseDict):
+	"""
+	Converts a alphabetic letter to morse code string value
+	"""
 	morseList = []
 	for letter in word:
 		morseLetter = morseDict.get(letter.lower())
@@ -56,6 +82,9 @@ def transcodeWord(word, morseDict):
 	return morseList
 
 def playMorseSentence(sentence):
+	"""
+	Plays an alphabetic sentence in morse code
+	"""
 	alphabetWords = sentence.split()
 	morseDict = decodeMorse('codes.json')
 	for word in alphabetWords:
